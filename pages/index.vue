5011d875e99b17c07e39ef0b50367a03c5b12680
hiracky16 <h.piiice16@gmail.com>
2020-05-02 19:37:12 +0900

    home に変更

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@ export default class Index extends Vue {
     } catch (error) {
       console.log(error)
     } finally {
-      this.$router.push('user')
+      this.$router.push('home')
     }
   }
 }
