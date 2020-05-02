f285d09f7d17777f77bf4598a6fb28052d6ba3e6
yoshifuji <t.yoshifuji@me.com>
2020-05-02 18:04:33 +0900

    トーナメントのデザイン

---
+++ components/TournamentGame.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="TournamentGame">
+    <div class="TournamentGame__player">
+      {{ game.player1.name }}
+    </div>
+    <div v-if="game.player2" class="TournamentGame__player">
+      {{ game.player2.name }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'nuxt-property-decorator'
+import { Game } from '~/models/tournament'
+
+@Component({})
+export default class TournamentGame extends Vue {
+  @Prop({
+    type: Object,
+    required: true
+  })
+  game!: Game
+}
+</script>
+
+<style lang="scss" scoped>
+.TournamentGame {
+  &__player {
+    padding: 5px 10px;
+    font-size: 18px;
+    width: 160px;
+    text-align: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+}
+</style>
