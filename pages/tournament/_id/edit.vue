07f778c6e7fd4e80ee7bd804d431e4bb29641724
yoshifuji <t.yoshifuji@me.com>
2020-05-06 17:04:15 +0900

    userTournamenntに統一

--- pages/tournament/_id/edit.vue
+++ pages/tournament/_id/edit.vue
@@ -6,23 +6,21 @@
 import Vue from 'vue'
 import { Component, namespace } from 'nuxt-property-decorator'
 import TournamentEditTemplate from '~/components/templates/tournament/_id/TournamentEditTemplate.vue'
-import { Game } from '~/models/tournament'
-import { sampleRounds } from '~/tmp/sample_rounds'
 
 const RoundStore = namespace('rounds')
 
 @Component({
   components: { TournamentEditTemplate },
-  fetch ({ store }) {
-    store.dispatch('rounds/setInitialRounds', sampleRounds)
+  fetch ({ store, params }) {
+    store.dispatch('rounds/fetchUserTournament', params.id)
   }
 })
 export default class TournamentEdit extends Vue {
-  @RoundStore.Getter('rounds')
-  rounds!: { games: Game[] }[]
+  @RoundStore.Getter('userTournament')
+  tournament!: any
 
-  mounted () {
-    console.log(this.rounds)
+  get rounds () {
+    return this.tournament.rounds
   }
 }
 </script>
