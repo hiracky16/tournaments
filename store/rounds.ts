f68354ab2da9929899eac54ea5645866744fab5e
yoshifuji <t.yoshifuji@me.com>
2020-05-09 16:30:34 +0900

    下層で勝敗をかえると上層のplayerが変更されるようにした

--- store/rounds.ts
+++ store/rounds.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { ActionTree, GetterTree, MutationTree } from 'vuex'
-import { Game, GameParams, PlayerKeys } from '~/models/tournament'
+import { Game, UpdateGameParams, UpdateNextGamePlayerParams, PlayerKeys } from '~/models/tournament'
 import User from '~/models/User'
 
 interface Round {
@@ -23,14 +23,15 @@ export const getters: GetterTree<RootState, RootState> = {
 
 const SET_USER_TOURNAMENT = 'SET_USER_TOURNAMENT'
 const UPDATE_GAME_WINNER = 'UPDATE_GAME_WINNER'
+const UPDATE_NEXT_GAME_PLAYER = 'UPDATE_NEXT_GAME_PLAYER'
 
 export const mutations: MutationTree<RootState> = {
   // FIXME: type
   [SET_USER_TOURNAMENT] (state: State, tournament: any) {
     state.userTournament = tournament
   },
-  [UPDATE_GAME_WINNER] (state: State, gameParams: GameParams) {
-    const { roundIdx, gameIdx, game } = gameParams
+  [UPDATE_GAME_WINNER] (state: State, gameParams: UpdateGameParams) {
+    const { gameIdx, roundIdx, game } = gameParams
     const winnerKey = Object.keys(game).filter((player) => {
       return game[player as PlayerKeys].winner
     })
@@ -46,10 +47,30 @@ export const mutations: MutationTree<RootState> = {
 
     const currentRoundGames = state.userTournament.rounds.slice(-roundIdx - 1)[0].games
     const nextGame = state.userTournament.rounds.slice(-roundIdx)[0].games[nextGameIdx]
+    const nextGamePlayer = nextGame[nextPlayerKey]
 
     Vue.set(currentRoundGames, gameIdx, game)
-    Vue.set(nextGame[nextPlayerKey], 'id', winnerPlayer.id)
-    Vue.set(nextGame[nextPlayerKey], 'name', winnerPlayer.name)
+    Vue.set(nextGamePlayer, 'id', winnerPlayer.id)
+    Vue.set(nextGamePlayer, 'name', winnerPlayer.name)
+  },
+  [UPDATE_NEXT_GAME_PLAYER] (state: State, updateGameParams: UpdateNextGamePlayerParams) {
+    const { gameIdx, roundIdx, player } = updateGameParams
+
+    let nextPlayerKey: PlayerKeys
+    if (gameIdx % 2 === 0) {
+      nextPlayerKey = 'player1'
+    } else {
+      nextPlayerKey = 'player2'
+    }
+
+    const nextGameIdx = Math.floor(gameIdx / 2)
+    const nextGame = state.userTournament.rounds.slice(-roundIdx)[0].games[nextGameIdx]
+    const nextPlayer = nextGame[nextPlayerKey]
+
+    if (player.winner && nextPlayer.id !== '' && nextPlayer.id !== player.id) {
+      Vue.set(nextPlayer, 'id', player.id)
+      Vue.set(nextPlayer, 'name', player.name)
+    }
   }
 }
 
@@ -62,7 +83,10 @@ export const actions: ActionTree<RootState, RootState> = {
     const tournament = await user.findUserTournamentById(id)
     commit(SET_USER_TOURNAMENT, tournament)
   },
-  updateGameWinner ({ commit }, gameParams: GameParams) {
+  updateGameWinner ({ commit }, gameParams: UpdateGameParams) {
     commit(UPDATE_GAME_WINNER, gameParams)
+  },
+  updateNextGamePlayer ({ commit }, gameParams: UpdateNextGamePlayerParams) {
+    commit(UPDATE_NEXT_GAME_PLAYER, gameParams)
   }
 }
