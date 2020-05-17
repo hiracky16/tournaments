f1e050f1ebb55c6bd6a91b0feabac2d0fc719d5e
domi10momo <hb.kmaia93@icloud.com>
2020-05-17 18:39:15 +0900

    トーナメント組数表記

--- components/TournamentList.vue
+++ components/TournamentList.vue
@@ -3,7 +3,7 @@
     <section>
       <nav class="panel">
         <p class="panel-heading">
-          トーナメント一覧
+          トーナメント一覧  (選手数：無作為32組)
         </p>
         <div
           v-for="tournament in tournaments"
