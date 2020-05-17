16bac4894c8cf799eea65533921f116da9a74ffd
hiracky16 <h.piiice16@gmail.com>
2020-05-17 14:29:36 +0900

    ogp 返す cloud functions 完成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions'
 // const fs = require('fs')
 const Twitter = require('twitter')
-// const admin = require('firebase-admin')
-// const db = admin.firestore()
+const admin = require('firebase-admin')
+admin.initializeApp()
+const db = admin.firestore()
+
+const DOMAIN = 'https://akbtest-66d57.web.app'
 
 const client = new Twitter({
   consumer_key: functions.config().credential.twitter.api_key,
@@ -26,59 +29,81 @@ export const tweets = functions.firestore.document('users/{userId}/tweets/{tweet
   })
 })
 
-export const ogp = functions.region('us-central1').https.onRequest((req, res) => {
+export const ogp = functions.region('us-central1').https.onRequest(async (req, res) => {
   const userAgent = req?.headers['user-agent']?.toLowerCase()
-  const path = req.params[0].split('/')
-  // const domain = 'https://akbtest-66d57.web.app'
-  // const indexHTML = fs.readFileSync('./hosting/index.html').toString()
+  const path = req.path
+  const params = path.match(/^\/ogp\/user\/(.+?)\/tournament\/(.+?)$/)
+  const tournamentId = params?.pop()
+  const userId = params?.pop()
+  const userDoc = await db.collection('users').doc(userId).get()
+  const doc = await db.collection(`users/${userId}/tournaments`).doc(tournamentId).get()
+  const tournamentName = doc.data()?.name
+  const description = createDescription(userDoc.data().name, tournamentName, doc.data()?.rounds)
+  // TODO: firestorage に保存したものにする
+  const imageUrl = 'https://img.benesse-cms.jp/pet-dog/item/image/normal/resized/resized_a18dcd9f-548b-4f68-ab7f-14887d74e7f5.jpg'
 
   const isBot = userAgent !== null && (userAgent?.includes('googlebot') || userAgent?.includes('twitterbot'))
 
   if (isBot) {
-    const html = createHtmlForTwitter(path)
-    console.log(html)
+    const html = createHtml(tournamentName, description, imageUrl, userId, tournamentId)
     res.status(200).send(html)
   } else {
-    // const url = `https://akbtest-66d57.web.app/${path.join('/')}`
-    const html = createHtml(path)
-    console.log(html)
-    res.status(200).send(html)
+    const url = `${DOMAIN}/user/${userId}/tournament/${tournamentId}`
+    res.redirect(url)
   }
 })
 
-const createHtmlForTwitter = (path: any) => {
+type Player = {
+  id: string
+  name: string
+  winner: boolean
+}
+
+type Game = {
+  player1: Player
+  player2: Player
+}
+
+const createDescription = (
+  userName: string,
+  tournamentName: string,
+  rounds: Game[]) => {
+  const desc = `${userName}さんは${tournamentName}で遊びました！`
+  const lastGame = rounds.pop()
+  if (lastGame?.player1?.winner) {
+    const winPlayer = lastGame?.player1?.winner ? lastGame?.player1 : lastGame?.player2
+    return `${desc}\n勝者は「${winPlayer.name}」です。`
+  } else {
+    return desc
+  }
+}
+
+const createHtml = (
+  title: string,
+  description: string,
+  imageUrl: string,
+  userId: string | undefined,
+  tournamentId: string | undefined) => {
   return `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>Wishlist Share</title>
-    <meta property="og:title" content="ogp のテスト" />
-    <meta property="og:image" content="https://img.benesse-cms.jp/pet-dog/item/image/normal/resized/resized_a18dcd9f-548b-4f68-ab7f-14887d74e7f5.jpg" />
+    <title>Uniqa</title>
+    <meta property="og:title" content="${title}" />
+    <meta property="og:image" content="${imageUrl}" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
-    <meta property="og:description" content="ogp のテストです" />
-    <meta property="og:url" content="https://akbtest-66d57.web.app/${path.join('/')}" />
+    <meta property="og:description"${description}" />
+    <meta property="og:url" content="${DOMAIN}/ogp/user/${userId}/tournament/${tournamentId}" />
     <meta property="og:type" content="article" />
-    <meta property="og:site_name" content="uniqa" />
-    <meta name="twitter:site" content="https://akbtest-66d57.web.app" />
+    <meta property="og:site_name" content="Uniqa" />
+    <meta name="twitter:site" content="${DOMAIN}" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="ogp のテスト" />
-    <meta name="twitter:image" content="https://img.benesse-cms.jp/pet-dog/item/image/normal/resized/resized_a18dcd9f-548b-4f68-ab7f-14887d74e7f5.jpg" />
-    <meta name="twitter:description" content="ogp のテスト" />
+    <meta name="twitter:title" content="${title}" />
+    <meta name="twitter:image" content="${imageUrl}" />
+    <meta name="twitter:description" content="${description}" />
   </head>
   <body>For Twitter Bot</body>
-</html>
-`
-}
-
-const createHtml = (path: any) => {
-  return `<!DOCTYPE html>
-<html>
-  <body>
-   <script>
-   window.location="https://akbtest-66d57.web.app/user/m4gaqEpz4NgrISY0DnupCLRIjzi2/tournament/3bgp2nmbut8"
-   </script>
-  </body>
 </html>
 `
 }
